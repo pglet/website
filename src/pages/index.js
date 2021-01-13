787c169f27471015af423548b74db5f60ec10b4c
Feodor Fitsner <feodor@appveyor.com>
2021-01-13 09:38:32 -0800

    Index page updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: <>Easy to Use</>,
+    title: <>Build web apps in your language</>,
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
@@ -18,7 +18,7 @@ const features = [
     ),
   },
   {
-    title: <>Focus on What Matters</>,
+    title: <>No HTML/CSS/JavaScript required</>,
     imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: (
       <>
@@ -28,7 +28,7 @@ const features = [
     ),
   },
   {
-    title: <>Powered by React</>,
+    title: <>Zero deployment</>,
     imageUrl: 'img/undraw_docusaurus_react.svg',
     description: (
       <>
