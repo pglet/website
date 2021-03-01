70ab3948e86ae7d1da2559da275d6d19332104f7
Feodor Fitsner <feodor@appveyor.com>
2021-02-28 17:30:12 -0800

    Pglet 0.2.3

--- static/pglet.sh
+++ static/pglet.sh
@@ -1,5 +1,5 @@
 # Constants
-PGLET_VER="0.2.2"                             # Pglet version required by this script
+PGLET_VER="0.2.3"                             # Pglet version required by this script
 PGLET_DEFAULT_INSTALL_DIR="$HOME/.pglet/bin"  # Default installation directory
 
 # Installation variables
