a8ce6a513ad34f298b9f3b4c5b8a55d6b5e83697
Feodor Fitsner <feodor@appveyor.com>
2022-01-09 10:16:00 -0800

    BrowserOnly inside <div>

--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -39,31 +39,32 @@ export default function SignupForm() {
     }, [token, email]);
 
     return (
-        <BrowserOnly fallback={<div>Loading...</div>}>
-            {() => {
-                const signupConfirmed = window.location.href.endsWith('?signup-confirmed')
-                return <div id="signup" className="signup-form">
-                {!token && !signupConfirmed && <form onSubmit={onSubmit}>
-                    <h3>Subscribe to Pglet newsletter for project updates and tutorials!</h3>
-                    <input
-                        type="email"
-                        value={email}
-                        placeholder="Your email address"
-                        onChange={(evt) => setEmail(evt.target.value)}
-                    />
-                    <input type="submit" value="Submit" />
-                    <HCaptcha
-                        sitekey="450cf388-4fd0-4485-a786-dd0129a596ed"
-                        size="invisible"
-                        onVerify={setToken}
-                        onError={onError}
-                        onExpire={onExpire}
-                        ref={captchaRef}
-                    />
-                </form>}
-                {token && !signupConfirmed && <div>Thank you! You will receive the confirmation email shortly.</div>}
-                {signupConfirmed && <div><span style={{fontSize:'25px', marginRight:'10px'}}>🎉</span>Congratulations! You have successfully subscribed to Pglet newsletter.</div>}
-            </div>}}
-        </BrowserOnly>
+        <div id="signup" className="signup-form">
+            <BrowserOnly fallback={<div>Loading...</div>}>
+                {() => {
+                    const signupConfirmed = window.location.href.endsWith('?signup-confirmed')
+                    return !token && !signupConfirmed && <form onSubmit={onSubmit}>
+                        <h3>Subscribe to Pglet newsletter for project updates and tutorials!</h3>
+                        <input
+                            type="email"
+                            value={email}
+                            placeholder="Your email address"
+                            onChange={(evt) => setEmail(evt.target.value)}
+                        />
+                        <input type="submit" value="Submit" />
+                        <HCaptcha
+                            sitekey="450cf388-4fd0-4485-a786-dd0129a596ed"
+                            size="invisible"
+                            onVerify={setToken}
+                            onError={onError}
+                            onExpire={onExpire}
+                            ref={captchaRef}
+                        />
+                    </form>
+                    {token && !signupConfirmed && <div>Thank you! You will receive the confirmation email shortly.</div>}
+                    {signupConfirmed && <div><span style={{fontSize:'25px', marginRight:'10px'}}>🎉</span>Congratulations! You have successfully subscribed to Pglet newsletter.</div>}}
+                }
+            </BrowserOnly>
+        </div>
     );
 }
